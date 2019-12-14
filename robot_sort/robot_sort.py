eb9a39d1b6f47e07954ccc0fb293fd766d4edbf7
TheHaunted <kevin_lopez@live.com>
2019-12-13 19:43:06 -0600

    MVP

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,7 +97,27 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        pass
+
+        while self.light_is_on() == False:
+            self.set_light_on()
+            self.swap_item()
+
+            while self.can_move_right():
+                self.move_right()
+                if self.compare_item() <= 0:
+                    self.swap_item()
+                else:
+                    self.set_light_off()
+
+            self.swap_item()
+
+            while self.can_move_left():
+                self.move_left()
+                if self.compare_item() == None or self.compare_item() >= 0:
+                    self.swap_item()
+
+        
+
 
 
 if __name__ == "__main__":
